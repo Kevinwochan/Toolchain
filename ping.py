d5f01185fd552bbd8097c04e9468fe53ba2541b5
kevinwochan <=>
2020-12-19 13:31:33 -0500

    working async ping

--- ping.py
+++ ping.py
@@ -11,8 +11,11 @@ import mtrpacket
 #
 async def probe(host):
     async with mtrpacket.MtrPacket() as mtr:
-        result = await mtr.probe(host)
-
+        try:
+            result = await mtr.probe(host)
+        except asyncio.CancelledError:
+            print(f'task timeout {host}')
+            return
         #  If the ping got a reply, report the IP address and time
         if result.success:
             print('reply from {} in {} ms'.format(
@@ -20,22 +23,39 @@ async def probe(host):
         else:
             print('no reply ({})'.format(result.result))
 
+async def run(subnet, subnet_mask):
+    tasks = [asyncio.create_task(probe(f'{subnet}.{i}')) for i in range(0, 254)]
+    running_tasks = asyncio.shield(asyncio.gather(*tasks))
+    try: 
+        await asyncio.wait_for(running_tasks, 10)
+    except asyncio.TimeoutError:
+        print('some host names did not reply within 10s')
+
 
 #  Get a hostname to ping from the commandline
-if len(sys.argv) > 1:
+if len(sys.argv) == 2:
     hostname = sys.argv[1]
+    #  We need asyncio's event loop to run the coroutine
+    loop = asyncio.get_event_loop()
+    try:
+        probe_coroutine = probe(hostname)
+        try:
+            loop.run_until_complete(probe_coroutine)
+        except mtrpacket.HostResolveError:
+            print("Can't resolve host '{}'".format(hostname))
+    finally:
+        loop.close()
+
+# Ip address range given
+elif len(sys.argv) == 3:
+    subnet = '.'.join(sys.argv[1].split('.')[:-1])
+    subnet_mask = sys.argv[2]
+    # TODO: calc subnet format into ip address range
+    asyncio.run(run(subnet, subnet_mask))
+
 else:
     print('Usage: python3 ping.py <hostname>')
     sys.exit(1)
 
 
-#  We need asyncio's event loop to run the coroutine
-loop = asyncio.get_event_loop()
-try:
-    probe_coroutine = probe(hostname)
-    try:
-        loop.run_until_complete(probe_coroutine)
-    except mtrpacket.HostResolveError:
-        print("Can't resolve host '{}'".format(hostname))
-finally:
-    loop.close()
+
